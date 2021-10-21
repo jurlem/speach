4a2b35371d5aee2616fa2024664d82eb34a987f6
Merle Jansen <merlevanjansen@gmail.com>
2021-10-21 14:01:31 +0300

    adding footer

--- components/TextToSpeech.tsx
+++ components/TextToSpeech.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { NextPage } from 'next';
 
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -10,10 +11,12 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
-const TextToSpeech = () => {
+const TextToSpeech: NextPage = () => {
   const [selectedLanguage, setSelectedLanguage] = useState('en-EN');
 
   const handleChange = (e: SelectChangeEvent) => {
@@ -37,41 +40,66 @@ const TextToSpeech = () => {
   }
 
   return (
-    <Stack spacing={2}>
-      <Typography variant="h4" component="h1" align="center" gutterBottom>Speech to Text </Typography>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+      }}
+    >
+      <Stack component="main" spacing={2}>
+        <Typography variant="h2" component="h1" align="center" mt={8} gutterBottom>Speech to Text </Typography>
 
-      <Box sx={{ maxWidth: 150 }}>
-        <FormControl size='small' >
-          <InputLabel id="demo-simple-select-label">Language</InputLabel>
-          <Select
-            defaultValue={'en-EN'}
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={selectedLanguage}
-            label="Lang"
-            onChange={handleChange}
-          >
-            <MenuItem value={'en-EN'}>English</MenuItem>
-            <MenuItem value={'nl-NL'}>Dutch</MenuItem>
-          </Select>
-        </FormControl>
+        <Box sx={{ maxWidth: 150 }}>
+          <FormControl size='small' >
+            <InputLabel id="demo-simple-select-label">Language</InputLabel>
+            <Select
+              defaultValue={'en-EN'}
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={selectedLanguage}
+              label="Lang"
+              onChange={handleChange}
+            >
+              <MenuItem value={'en-EN'}>English</MenuItem>
+              <MenuItem value={'nl-NL'}>Dutch</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+
+        <Typography variant="body1" color={listening ? "text.warning" : "text.secondary"}>Microphone: {listening ? 'on' : 'off'}</Typography>
+        <ButtonGroup variant="outlined" aria-label="outlined button group">
+          <Button onClick={() => onStart()}>Start</Button>
+          <Button onClick={SpeechRecognition.stopListening}>Stop</Button>
+          <Button onClick={resetTranscript}>Reset</Button>
+        </ButtonGroup>
+
+        <TextField
+          rows={4}
+          value={transcript}
+          id="outlined-textarea"
+          placeholder="..."
+          multiline
+        />
+      </Stack>
+      <Box
+        component="footer"
+        sx={{
+          py: 3,
+          px: 2,
+          mt: 'auto',
+        }}
+      >
+        <Container >
+          <Typography variant="body2">
+            Merle Jansen
+          </Typography>
+          <Link color="inherit" href="https://github.com/jurlem/speach/tree/main">
+            Github
+          </Link>{' '}
+        </Container>
       </Box>
-
-      <Typography color={listening ? "text.warning" : "text.secondary"}>Microphone: {listening ? 'on' : 'off'}</Typography>
-      <ButtonGroup variant="outlined" aria-label="outlined button group">
-        <Button onClick={() => onStart()}>Start</Button>
-        <Button onClick={SpeechRecognition.stopListening}>Stop</Button>
-        <Button onClick={resetTranscript}>Reset</Button>
-      </ButtonGroup>
-
-      <TextField
-        rows={4}
-        value={transcript}
-        id="outlined-textarea"
-        placeholder="..."
-        multiline
-      />
-    </Stack>
+    </Box>
   );
 };
 export default TextToSpeech;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import 'regenerator-runtime/runtime'
 import React from "react";
+import { NextPage } from 'next';
+
 import TextToSpeech from '../components/TextToSpeech'
 import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
 
-
-
-const Home = () => {
+const Home: NextPage = () => {
   return (
     <Container maxWidth="md">
+      <CssBaseline />
       <TextToSpeech />
     </Container >
   )
